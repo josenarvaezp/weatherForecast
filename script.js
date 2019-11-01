06858a2f1852c55c1fe473bb4628d7776534de09
Jose Narvaez Paliza <josenarvaezpaliza@eduroam-student-202-29.lut.ac.uk>
2019-11-01 00:11:49 +0000

    Make better

--- script.js
+++ script.js
@@ -11,15 +11,13 @@ function get_weather_stats(weatherData,cnt){
     information.  
         
     */
-
     //setting initial day and max temp according to first JSON row of weather data 
     var currentDate = new Date(weatherData[0].dt*1000);
     var maxTemp = weatherData[0].main.temp_max;
     var totalHumidity = 0;
     var counter = 0; // number of rows for each day. 
 
-    for (var row in weatherData){
-
+    for(var row in weatherData){
         //getting data of current row
         var timeStamp = weatherData[row].dt;
         var date = new Date(timeStamp*1000);
@@ -30,6 +28,7 @@ function get_weather_stats(weatherData,cnt){
         if (date.getDate() === currentDate.getDate() && date.getMonth() === currentDate.getMonth() && date.getFullYear() === currentDate.getFullYear()){
             //updating max temp
             if (maxTemp < temp ) maxTemp = temp;
+
             //increase humidity
             totalHumidity += humidity;
             
@@ -37,18 +36,17 @@ function get_weather_stats(weatherData,cnt){
 
             //if current row is the last row (data from the last day) then append values to end of the table
             if (row == cnt-1){
-                var avgHumidity = totalHumidity / counter;
+                var avgHumidity = totalHumidity/counter;
                 avgHumidity = avgHumidity.toPrecision(5);
                 var strDate = currentDate.getDate() + '/'+ (currentDate.getMonth()+1)+'/'+ currentDate.getFullYear();
                 $('#weatherTable').append('<tr><td>'+strDate+'</td><td>'+maxTemp+'</td><td>'+ avgHumidity+'</td></tr>');
             }
 
         }else{//data from different day
-
             //append values to table
-            var avgHumidity = totalHumidity / counter;
+            var avgHumidity = totalHumidity/counter;
             avgHumidity = avgHumidity.toPrecision(5);
-            var strDate = currentDate.getDate() + '/'+ (currentDate.getMonth()+1)+'/'+ currentDate.getFullYear();
+            var strDate = currentDate.getDate()+'/'+(currentDate.getMonth()+1)+'/'+currentDate.getFullYear();
             $('#weatherTable').append('<tr><td>'+strDate+'</td><td>'+maxTemp+'</td><td>'+ avgHumidity+'</td></tr>');
 
             //update values
